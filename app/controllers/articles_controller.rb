2e3695882f437d7aa2b59b765e3a9f1bcbb3237b
Robin Nilsson <snokstyle@gmail.com>
2013-08-16 16:51:50 +0200

    Add fully functional CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,6 @@ class ArticlesController < ApplicationController
 		@articles = Article.all
 	end
 
-	def show
-		@article = Article.find(params[:id])
-	end
-
 	def new
 		@article = Article.new
 	end
@@ -17,6 +13,30 @@ class ArticlesController < ApplicationController
 	  @article.save
 
 	  redirect_to article_path(@article)
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+
+		redirect_to action: 'index'
 	end
 
 	def article_params
